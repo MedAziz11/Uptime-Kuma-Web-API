e68183ec628bf3908a6fcd31a9869445d4632de5
nstankov-bg <me@nstankov.com>
2023-04-10 06:38:28 -0400

    borked router name

--- app/main.py
+++ app/main.py
@@ -3,8 +3,18 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 from routers import (
-    statuspage, monitor, auth, tags, cert, info, uptime,
-    ping, database, settings as settings_router, user, maintenance
+    statuspage,
+    monitor,
+    auth,
+    tags,
+    cert,
+    info,
+    uptime,
+    ping,
+    database,
+    settings as user,
+    settings,
+    maintenance,
 )
 from config import settings as app_settings
 from app_setup import initialize_app
@@ -13,7 +23,7 @@ app = FastAPI(title=app_settings.PROJECT_NAME)
 app.router.redirect_slashes = True
 
 app.include_router(user.router, prefix="/users", tags=["Users"])
-app.include_router(settings_router.router, prefix="/settings", tags=["Settings"])
+app.include_router(settings.router, prefix="/settings", tags=["Settings"])
 app.include_router(database.router, prefix="/database", tags=["DataBase"])
 app.include_router(monitor.router, prefix="/monitors", tags=["Monitor"])
 app.include_router(statuspage.router, prefix="/statuspages", tags=["Status Page"])
@@ -25,10 +35,12 @@ app.include_router(ping.router, prefix="/ping", tags=["Ping Average"])
 app.include_router(uptime.router, prefix="/uptime", tags=["Uptime"])
 app.include_router(auth.router, prefix="/login", tags=["Authentication"])
 
+
 @app.on_event("startup")
 async def startup_event():
     await initialize_app(app)
 
+
 @app.get("/", include_in_schema=False)
 async def root():
-    return RedirectResponse(url='/docs')
+    return RedirectResponse(url="/docs")
